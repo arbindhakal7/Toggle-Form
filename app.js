5a6c19f99d0655fe93bfc28e89ec4534deec1333
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-19 23:07:18 +0545

    :zap: eventlistner for toggle

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const signupBtn = document.querySelector('.SignUpBtn')
 const loginBtn = document.querySelector('.SignInBtn')
+const moveBtn = document.querySelector('.moveBtn')
+
+loginBtn.addEventListener("click",()=>{
+    moveBtn.classList.add("rightBtn")
+})
+
+signupBtn.addEventListener("click",()=>{
+    moveBtn.classList.remove("rightBtn")
+})
