d2def1065c0ef413726f8aaf2e0972d311520eae
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-19 23:17:07 +0545

    :zap: targeted login and signup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const signupBtn = document.querySelector('.SignUpBtn')
 const loginBtn = document.querySelector('.SignInBtn')
 const moveBtn = document.querySelector('.moveBtn')
+const signup = document.querySelector(".signup")
+const login = document.querySelector(".login")
+
 
 loginBtn.addEventListener("click",()=>{
     moveBtn.classList.add("rightBtn")
